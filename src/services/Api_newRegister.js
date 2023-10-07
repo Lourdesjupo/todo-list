9d07258dda19e1e8c2b78768ba54d5df49b48fa7
Lourdesjupo <lourdes.jupo@gmail.com>
2023-10-07 16:42:37 +0200

    add localStore for register

--- src/services/Api_newRegister.js
+++ src/services/Api_newRegister.js
@@ -1,10 +1,14 @@
 const newRegister = async (data) => {
   try {
-    await fetch(`${import.meta.env.VITE_TDLIST_API}/api/register`, {
+    const response = await fetch(`${import.meta.env.VITE_TDLIST_API}/api/register`, {
       method: 'POST',
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify(data),
     });
+    const dataResponse = await response.json();
+        localStorage.removeItem('jwt')
+        localStorage.setItem('jwt', JSON.stringify(dataResponse.token));
+        localStorage.setItem('name', JSON.stringify(dataResponse.user));
       window.location.href = `${import.meta.env.VITE_TDLIST}#tasks`;
     return (window.location.href);
   } catch (error) {
