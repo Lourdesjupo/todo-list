aaf94395e4f8ce74a5d34fcb0d50fd97ef92c268
Lourdesjupo <lourdes.jupo@gmail.com>
2023-12-02 13:10:28 +0100

    change the connection to a connection pool

--- server/src/index.js
+++ server/src/index.js
@@ -22,31 +22,40 @@ server.use(cookieParser());
 
 const port = process.env.PORT;
 console.log(port);
-let connect;
+server.listen(port, () => {
+  console.log(`servidor arrancado: http://localhost:${port}`);
+});
 
-(async ()=>{
-   connect = await connectDb();
-  
-  server.listen(port, () => {
-    console.log(`servidor arrancado: http://localhost:${port}`);
-  });
-  
-})();
+//conection pool
+// Create the connection pool. The pool-specific settings are the defaults
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
 
 //conexión a BBDD
 
-async function connectDb() {
-  const connection = await mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-  });
+// async function connectDb() {
+//   const connection = await mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE,
+//   });
 
-  await connection.connect();
-  console.log(`Conectada a la BBDD ${connection.threadId}`);
-  return connection;
-}
+//   await connection.connect();
+//   console.log(`Conectada a la BBDD ${connection.threadId}`);
+//   return connection;
+// }
 
 //JWT generate and verify Tokens
 const key = process.env.KEY;
@@ -89,13 +98,13 @@ server.get('/api/allTasks', async (req, res) => {
     res.json('login no es correcto');
     return;
   }
-  const connect = await connectDb();
+
   console.log(decoded, 'decoded');
   const id = decoded.id;
   const allTasks = 'select * from todolist_todo where fk_user_id = ?';
-  const [tasks] = await connect.query(allTasks, [id]);
+  const [tasks] = await pool.query(allTasks, [id]);
   console.log(tasks);
-  connect.end();
+
   res.json(tasks);
   //console.log(typeof(tasks[4].task_date), tasks[4].task_date, tasks[4].task_id)
 });
@@ -105,9 +114,8 @@ server.get('/api/allTasks', async (req, res) => {
 server.get('/api/getTask/:id', async (req, res) => {
   const id = req.params.id;
   const queryTaskId = 'select * from todolist_todo where task_id = ?';
-  const connect = await connectDb();
-  const [result] = await connect.query(queryTaskId, [id]);
-  connect.end();
+  const [result] = await pool.query(queryTaskId, [id]);
+
   res.json(result);
 });
 
@@ -127,14 +135,14 @@ server.post('/api/addNewTask', async (req, res) => {
   console.log('holi', decoded);
   const isertQuery =
     'INSERT INTO todolist_todo (task_name, task_date, task_checked, fk_user_id) VALUES (?,?,?,?)';
-  const connected = await connectDb();
-  const [result] = await connected.query(isertQuery, [
+
+  const [result] = await pool.query(isertQuery, [
     req.body.name,
     req.body.date,
     req.body.checked,
     id,
   ]);
-  connected.end();
+
   res.json(result);
   console.log('esta es la respuesta del post', result);
 });
@@ -154,13 +162,12 @@ server.put('/api/editItem', async (req, res) => {
   const item = req.body;
   const queryUpdate =
     'UPDATE todolist_todo  SET  task_name = ?, task_date = ? WHERE task_id = ?';
-  const connection = await connectDb();
-  const [result] = await connection.query(queryUpdate, [
+  const [result] = await pool.query(queryUpdate, [
     item.name,
     item.date,
     item.id,
   ]);
-  connection.end();
+
   res.json(result);
 });
 
@@ -170,12 +177,9 @@ server.put('/api/taskchecked', async (req, res) => {
   console.log(data.check);
   const queryUpdateCheck =
     'UPDATE todolist_todo SET task_checked=? where task_id=?';
-  const connection = await connectDb();
-  const [result] = await connection.query(queryUpdateCheck, [
-    data.check,
-    data.id,
-  ]);
-  connection.end();
+
+  const [result] = await pool.query(queryUpdateCheck, [data.check, data.id]);
+
   res.json(result);
 });
 
@@ -184,9 +188,9 @@ server.delete(`/api/deleteTask/:id`, async (req, res) => {
   const id = req.params.id;
   console.log(id, 'hola id');
   const deleteQuery = 'DELETE FROM todolist_todo where task_id= ?';
-  const connection = await connectDb();
-  const [result] = await connection.query(deleteQuery, [id]);
-  connection.end();
+
+  const [result] = await pool.query(deleteQuery, [id]);
+
   res.json(result);
 });
 
@@ -199,15 +203,13 @@ server.post('/api/register', async (req, res) => {
   const passwordHash = await bcrypt.hash(pass, 15);
   const addRegister =
     'INSERT INTO users_todo (user_name, user_mail, user_password) VALUES (?,?,?)';
-  const connect = await connectDb();
-  const [result] = await connect.query(addRegister, [
+
+  const [result] = await pool.query(addRegister, [
     username,
     mail,
     passwordHash,
   ]);
   console.log(result, 'ES EL RESULTADO');
-  connect.end();
-
 
   let user = {
     mail,
@@ -223,7 +225,7 @@ server.post('/api/register', async (req, res) => {
       res.json({
         msg: 'success',
         token: token,
-        user: username
+        user: username,
       });
     }
   });
@@ -238,8 +240,8 @@ server.post('/api/login', async (req, res) => {
 
   //user exist BBDD
   let verifyUserQuery = 'SELECT *FROM users_todo WHERE user_mail = ?';
-  
-  const [users, fields] = await connect.query(verifyUserQuery, [body.mail]);
+
+  const [users, fields] = await pool.query(verifyUserQuery, [body.mail]);
   const user = users[0];
   console.log(user, 'usuario');
 
@@ -264,7 +266,5 @@ server.post('/api/login', async (req, res) => {
   });
 
   console.log('VERIFY PASS', verifyPass);
-
-  connect.end();
   res.status(200).json({ token: token, user: user.user_name });
 });
